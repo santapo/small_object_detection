b838db84343c3d7467e164b86dcf6ee9ec77651d
Trong Tuan Nguyen <trtuann34@gmail.com>
2023-03-23 07:37:21 +0700

    init

---
+++ main.py
@@ -0,0 +1,12 @@
+
+
+
+class ObjectDetector:
+    def __init__(self):
+        ...
+
+    def predict(self, images: np.ndarray):
+        ...
+
+    def predict_slices(self, images: np.ndarray):
+        ...
