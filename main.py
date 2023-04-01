db514e3aad28b75c1537a6ff325df5dbe572875d
santapo <trtuann34@gmail.com>
2023-04-01 21:31:16 +0700

    predict_slices

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
+import numpy as np
+import torch
 
+from utils.utils import get_slices
 
 
 class ObjectDetector:
     def __init__(self):
         ...
 
-    def predict(self, images: np.ndarray):
+    def predict(self, images: list[np.ndarray]):
         ...
 
-    def predict_slices(self, images: np.ndarray):
-        ...
+    def predict_slices(self, image: np.ndarray):
+        slices = get_slices(image)
+        all_res = []
+        for (start_y, start_x), slice in slices:
+            res = self.predict(slice)
+            res[:, 0] += start_y
+            res[:, 1] += start_x
+            all_res.append(res)
+        all_res = np.concatenate(all_res, axis=0)
+        return all_res
