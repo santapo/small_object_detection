bb46bc5511470c9ac385327d609bb16e059b956d
santapo <trtuann34@gmail.com>
2023-04-27 11:00:25 +0000

    revamp

--- lib_utils/utils.py
+++ lib_utils/utils.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+from torchvision import ops
 
 
 def get_slices(image: np.ndarray, slice_width: int, slice_height: int, overlapped_ratio: float) -> list[np.ndarray]:
@@ -19,59 +20,20 @@ def get_slices(image: np.ndarray, slice_width: int, slice_height: int, overlappe
                   window.append(((y, x), image[y:y + slice_width, x:x + slice_height,:]))
     return window
 
-@jit(nopython=True)
-def get_slices_new(image: np.ndarray, slice_width: int, slice_height: int, overlapped_ratio: float) -> list[np.ndarray]:
-    h, w = image.shape[:2]
-    stepSizeY = int(slice_height - slice_height*overlapped_ratio)
-    stepSizeX = int(slice_width - slice_width*overlapped_ratio)
-    a = np.arange(0,h, stepSizeX)
-    a[-1] = h - slice_height
-    b = np.arange(0,w, stepSizeY)
-    b[-1] = w - slice_width
-    lst = [image[i:i+slice_height, j:j+slice_width, :] for i in a for j in b]
-    return lst
 
-def nmm(bboxes: np.ndarray, iou_thres: float):
-    selected_bboxes = []
+# @jit(nopython=True)
+# def get_slices_new(image: np.ndarray, slice_width: int, slice_height: int, overlapped_ratio: float) -> list[np.ndarray]:
+#     h, w = image.shape[:2]
+#     stepSizeY = int(slice_height - slice_height*overlapped_ratio)
+#     stepSizeX = int(slice_width - slice_width*overlapped_ratio)
+#     a = np.arange(0,h, stepSizeX)
+#     a[-1] = h - slice_height
+#     b = np.arange(0,w, stepSizeY)
+#     b[-1] = w - slice_width
+#     lst = [image[i:i+slice_height, j:j+slice_width, :] for i in a for j in b]
+#     return lst
 
-    x = bboxes[:, 0]
-    y = bboxes[:, 1]
-    w = bboxes[:, 2]
-    h = bboxes[:, 3]
-    confidence_scores = bboxes[:, 4]
-    class_labels = bboxes[:, 5]
-    areas = w * h
 
-    sorted_indices = np.argsort(confidence_scores)[::-1]
-    sorted_bboxes = bboxes[sorted_indices]
-
-    while len(sorted_bboxes) > 0:
-        current_bbox = sorted_bboxes[0]
-        selected_bboxes.append(current_bbox)
-
-        ious = compute_iou(current_bbox, sorted_bboxes[1:])
-
-        suppress_indices = np.where(ious > iou_thres)[0] + 1
-
-        sorted_bboxes = np.delete(sorted_bboxes, suppress_indices, axis=0)
-
-    selected_bboxes = np.stack(selected_bboxes)
-    return selected_bboxes
-
-def compute_iou(bbox, bboxes):
-    x1 = np.maximum(bbox[0], bboxes[:, 0])
-    y1 = np.maximum(bbox[1], bboxes[:, 1])
-    x2 = np.minimum(bbox[0] + bbox[2], bboxes[:, 0] + bboxes[:, 2])
-    y2 = np.minimum(bbox[1] + bbox[3], bboxes[:, 1] + bboxes[:, 3])
-    intersection_areas = np.maximum(0, x2 - x1) * np.maximum(0, y2 - y1)
-
-    bbox_area = bbox[2] * bbox[3]
-    bboxes_areas = bboxes[:, 2] * bboxes[:, 3]
-    union_areas = bbox_area + bboxes_areas - intersection_areas
-
-    ious = intersection_areas / union_areas
-
-    return ious
 def draw_bbox(image: np.ndarray, bboxes: np.ndarray) -> np.ndarray:
     img = image.copy()
     fontFace = cv2.FONT_HERSHEY_COMPLEX
--- main.py
+++ main.py
@@ -1,22 +1,25 @@
 import numpy as np
 import torch
 from retinaface.pre_trained_models import get_model as retinaface_get_model
+from torchvision import ops
 
 from lib_utils.utils import get_slices
 
 
 class ObjectDetector:
-    def __init__(self, model_name):
+    def __init__(self, model_name, device="cuda"):
         self.model_name = model_name
+        self.device = device
+
         if "yolo" in model_name:
             self.model = torch.hub.load('ultralytics/yolov5', model_name)
+            self.model.to(self.device)
         elif "retina" in model_name:
-            self.model = retinaface_get_model("resnet50_2020-07-20", max_size=2048)
+            self.model = retinaface_get_model("resnet50_2020-07-20", max_size=2048, device=self.device)
 
     def _predict(self, images: list[np.ndarray]):
         if "yolo" in self.model_name:
             result = self.model(images).pred
-            import ipdb; ipdb.set_trace()
         elif "retina" in self.model_name:
             tmp = self.model.predict_jsons(images)
             result = [instance["bbox"] + [instance["score"]] + [1.0] for instance in tmp \
@@ -26,7 +29,7 @@ class ObjectDetector:
 
     def predict(self, images: list[np.ndarray]):
         results = self._predict(images)
-        final_results = [result.cpu().numpy() for result in results]
+        final_results = [result.cpu() for result in results]
         return final_results
 
     def predict_slices(self, image: np.ndarray):
@@ -39,15 +42,23 @@ class ObjectDetector:
             res[:, 2] += start_x
             res[:, 3] += start_y
             all_res.append(res)
-        all_res = np.concatenate(all_res, axis=0)
-        return all_res
+        all_res = torch.cat(all_res, dim=0)
+        keep_idxs = ops.batched_nms(
+            all_res[:, :4],
+            all_res[:, 4],
+            all_res[:, 5],
+            0.1
+        )
+        res = all_res[keep_idxs]
+        return res.numpy()
 
 
 if __name__ == "__main__":
     import cv2
+
     from lib_utils.utils import draw_bbox
-    img = img = cv2.imread("tests/face.jpg")
-    detector = ObjectDetector("retinaface")
+    img = img = cv2.imread("tests/small-vehicles1.jpeg")
+    detector = ObjectDetector("yolov5s")
     slice_res = detector.predict_slices(img)
     # slice_res = detector.predict(img)[0]
     img = draw_bbox(img, slice_res)
